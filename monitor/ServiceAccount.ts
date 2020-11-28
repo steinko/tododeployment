4150b3e88bd13395c345c3f079383b86bf5b8953
stein korsveien <stein_korsveien@msn.com>
2020-11-28 04:42:17 +0100

    fix travis

--- monitor/ServiceAccount.ts
+++
@@ -1,7 +0,0 @@
-import * as k8s from "@pulumi/kubernetes"
-import {clusterProvider } from '../cluster'
-
-export const serviceAccount = new k8s.core.v1.ServiceAccount('monitor-serviceAccount', 
-                        { metadata:{ name: 'filebeat',
-                                     }}, 
-                         {provider: clusterProvider})
